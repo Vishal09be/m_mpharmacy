b665ccc0f090a3a8b5bc3cbaaa8562c3cb2e3d4e
Vishal09be <vishal09be@gmail.com>
2024-11-26 17:06:17 +0000

    new buildspec

---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already registered.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ProductForm(FlaskForm):
+    name = StringField('Medicine Name', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    stock = IntegerField('Stock', validators=[DataRequired()])
+    submit = SubmitField('Add Medicine')
+
+class UpdateProductForm(FlaskForm):
+    price = FloatField('New Price', validators=[DataRequired()])
+    stock = IntegerField('New Stock', validators=[DataRequired()])
+    submit = SubmitField('Update')
---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    cart_items = db.relationship('CartItem', backref='user', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=0)  # Ensure the stock column is included
+    cart_items = db.relationship('CartItem', backref='product', lazy=True, cascade="all, delete-orphan")
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
