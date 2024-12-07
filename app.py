934f9adb41d19d20e1dc21349afc986b525022c2
Vishal09be <vishal09be@gmail.com>
2024-12-07 22:44:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def payment_success():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(app.run(host="0.0.0.0", port=5000))
+    app.run(host="0.0.0.0", port=5000)
